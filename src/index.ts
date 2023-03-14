2b5ee91df500a914399be99b55f43c0f7556e736
Jace Halvorson <halvo561@umn.edu>
2023-03-14 03:11:38 -0500

    Style upgrades

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+const menu = document.getElementById( "menu" );
+
+function setActiveIndex( index:number ): void
+{
+   if ( menu != null )
+   {
+      menu.dataset.activeIndex = index.toString( );
+   }
+}
+
+var menuItems = document.getElementsByClassName( "card-wrapper" ) as HTMLCollectionOf<Element>;
+for ( var i = 0; i < menuItems.length; i++ )
+{
+   (function( index )
+   {
+      menuItems[ index ].addEventListener( "mouseover", function(){ setActiveIndex( index ) } );
+   })( i );
+}
