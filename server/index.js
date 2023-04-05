38cc1fa536b0b075d654e6b492e93f1cbff05ea4
Jace Halvorson <jacehalvorson33@gmail.com>
2023-04-05 01:27:06 -0500

    React frontend communicating with express backend to display the table

--- server/index.js
+++ server/index.js
@@ -1,41 +1,31 @@
+const express = require('express');
 const path = require('path');
-const fs = require('fs');
 
-import React from 'react';
-import ReactDOMServer from 'react-dom/server';
-import App from '../src/App';
-import NFLStats from '../src/components/NFLStats';
-import getStatTable from '../src/utils';
+async function getStatTable( ) {
+   try
+   {
+      const url = 'https://www.pro-football-reference.com/years/2022/passing.htm';
+      const response = await fetch( url );
+      const html = await response.text( );
+      
+      const parser = new DOMParser( );
+      const parsedHtml = parser.parseFromString( html, 'text/html' );
+     const table = parsedHtml.querySelector( 'table' );
+     console.log( table );
+     return table;
+   }
+   catch ( error )
+   {
+      console.error( 'Error: Could not get stat table - ' + error );
+   }
+}
 
 const PORT = process.env.PORT || 3006;
-
-const express = require('express');
 const app = express();
 
-app.get('/stat-table', (req, res) => {
-   const NFLStatsComponent = ReactDOMServer.renderToString(<NFLStats />);
-   console.log("stat-table");
-   // const statTable = getStatTable();
-   res.send(
-      '<h1>Hello World</h1>'
-   );
-});
-
-app.get('*', (req, res) => {
-   const reactApp = ReactDOMServer.renderToString(<App />);
-   console.log("stat-table");
-   
-   const indexFile = path.resolve('../public/index.html');
-   fs.readFile(indexFile, 'utf8', (err, data) => {
-      if (err) {
-         console.error('Something went wrong:', err);
-         return res.status(500).send('Oops, better luck next time!');
-      }
-   
-      return res.send(
-         data.replace('<div id="root"></div>', `<div id="root">${reactApp}</div>`)
-      );
-   });
+app.get('/api/stat-table', (req, res) => {
+   res.setHeader('Content-Type', 'text/html');
+   res.sendFile(path.resolve(__dirname, 'stats_table.html'));
 });
 
 app.use(express.static(path.resolve(__dirname, '..', 'public')));
@@ -43,3 +33,49 @@ app.use(express.static(path.resolve(__dirname, '..', 'public')));
 app.listen(PORT, () => {
    console.log(`Server is listening on port ${PORT}`);
 });
+
+// const path = require('path');
+// const fs = require('fs');
+
+// import React from 'react';
+// import ReactDOMServer from 'react-dom/server';
+// import App from '../src/App';
+// import NFLStats from '../src/components/NFLStats';
+// import getStatTable from '../src/utils';
+
+// const PORT = process.env.PORT || 3006;
+
+// const express = require('express');
+// const app = express();
+
+// app.get('/stat-table', (req, res) => {
+//    const NFLStatsComponent = ReactDOMServer.renderToString(<NFLStats />);
+//    console.log("stat-table");
+//    // const statTable = getStatTable();
+//    res.send(
+//       '<h1>Hello World</h1>'
+//    );
+// });
+
+// app.get('*', (req, res) => {
+//    const reactApp = ReactDOMServer.renderToString(<App />);
+//    console.log("stat-table");
+   
+//    const indexFile = path.resolve('../public/index.html');
+//    fs.readFile(indexFile, 'utf8', (err, data) => {
+//       if (err) {
+//          console.error('Something went wrong:', err);
+//          return res.status(500).send('Oops, better luck next time!');
+//       }
+   
+//       return res.send(
+//          data.replace('<div id="root"></div>', `<div id="root">${reactApp}</div>`)
+//       );
+//    });
+// });
+
+// app.use(express.static(path.resolve(__dirname, '..', 'public')));
+
+// app.listen(PORT, () => {
+//    console.log(`Server is listening on port ${PORT}`);
+// });
--- src/components/NFLStats.jsx
+++ src/components/NFLStats.jsx
@@ -1,14 +1,25 @@
-import React from "react";
-import { getStatTable } from "../utils";
+import React, {useState} from "react";
 import "../style/NFLStats.css";
+import { useEffect } from "react";
 
 function NFLStats( )
 {
-   // TODO Send a GET request to the server to get the NFL stats table
+   const [ table, setTable ] = useState( '' );
+   
+   useEffect( ( ) => {
+      // Send a GET request to the server to get the NFL stats table
+      console.log( 'Reading NFL stats from /api/stat-table' );
+      fetch( '/api/stat-table' )
+         .then( response => response.text() )
+         .then( text => setTable( text ) );
+   }, [ ] );
 
    return (
       <main id="nfl-stats">
          <h1>NFL Stats</h1>
+         <div dangerouslySetInnerHTML={{__html: table }}>
+
+         </div>
       </main>
    );
 }
