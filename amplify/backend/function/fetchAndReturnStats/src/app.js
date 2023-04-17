30d8dbb961e722764d99338407acf5a9ea4b5ffa
Jace Halvorson <jacehalvorson33@gmail.com>
2023-04-17 11:59:08 -0500

    Scrape Pro Footabll Reference on API call

--- amplify/backend/function/fetchAndReturnStats/src/app.js
+++ amplify/backend/function/fetchAndReturnStats/src/app.js
@@ -6,12 +6,10 @@ or in the "license" file accompanying this file. This file is distributed on an 
 See the License for the specific language governing permissions and limitations under the License.
 */
 
-
-
-
 const express = require('express')
 const bodyParser = require('body-parser')
 const awsServerlessExpressMiddleware = require('aws-serverless-express/middleware')
+const execFile = require('child_process').execFile;
 
 // declare a new express app
 const app = express()
@@ -25,18 +23,45 @@ app.use(function(req, res, next) {
   next()
 });
 
+/**********************
+ * Helper functions *
+ **********************/
+// Spawn child process to update JSON file, don't return until it's done
+async function spawnChildProcess( fileName, year, category ) {
+  const startTime = process.hrtime.bigint();
+  console.log( `spawnChildProcess() started` );
+
+  const result = await new Promise( ( resolve, reject ) => {
+     execFile( fileName, [ year, category ], function(err, data) {
+        if ( err ) {
+           reject( err );
+        }
+        else {
+           console.log( `nflstatsrequest.exe completed with return ${data.toString()}` );
+           resolve( data );
+        }
+     });
+  });
+
+  const elapsedTime = process.hrtime.bigint() - startTime;
+  console.log( `spawnChildProcess() finished in ${elapsedTime} microseconds` );
+  return result;
+}
+
 
 /**********************
  * Example get method *
  **********************/
 
-app.get('/nflstats/:year/:category', function(req, res) {
-  // Add your code here
-  res.json({success: 'True', url: req.url});
+app.get('/nflstats/:year/:category', async function(req, res) {
+  // Spawn child process and wait for execution to finish
+  await spawnChildProcess( 'request.exe', req.params.year, req.params.category );
+
+  // Now that JSON is updated, send it to the client
+  res.sendFile(path.resolve(__dirname, 'stats_table.json'));
 });
 
 app.get('/nflstats/:year/:category/*', function(req, res) {
-  // Add your code here
   res.json({error: 'Invalid parameters', url: req.url});
 });
 
