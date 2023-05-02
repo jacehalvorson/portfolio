7770c9f510b42393bca2e327561029ce8e17f51a
Jace Halvorson <jacehalvorson33@gmail.com>
2023-05-02 13:54:55 -0500

    Timeline draft

--- src/home/timeline.jsx
+++ src/home/timeline.jsx
@@ -8,18 +8,20 @@ function Timeline( props )
   return (
     <div id="timeline-wrapper" className={ getSectionWrapperClassNames( props.activeIndex, TIMELINE_INDEX ) }>
       <div id="timeline-content">
-        <div id="timeline-text">
-          <p>
-            I started programming in high school, where I learned the basics of Java and C++. I also learned the basics of web development, and I created a few websites for school projects.
-          </p>
-          <p>
-            I started studying Computer Science at the University of Waterloo in 2019. I've learned a lot about programming, and I've also learned a lot about the software development process.
-          </p>
-          <p>
-            I've worked on a few projects, including a 2D game engine, a mobile app, and a few websites. I've also worked on a few projects with other people, and I've learned a lot about working in a team.
-          </p>
+      <div class="timeline">
+        <div class="container left">
+          <div class="content">
+            <h2>2017</h2>
+            <p>Lorem ipsum..</p>
+          </div>
         </div>
-        <div id="timeline-picture" />
+        <div class="container right">
+          <div class="content">
+            <h2>2016</h2>
+            <p>Lorem ipsum..</p>
+          </div>
+        </div>
+      </div>
       </div>
     </div>
   )
