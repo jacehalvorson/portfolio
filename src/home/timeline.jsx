285c66c2a89a9c717c2ef3d7051e2aeb97311216
Jace Halvorson <jacehalvorson33@gmail.com>
2023-05-31 00:13:16 -0500

    New gradient and bulletpoints

--- src/home/timeline.jsx
+++ src/home/timeline.jsx
@@ -7,25 +7,32 @@ const timelineEventList = [
     employer: "Dairy Queen",
     startDate: "February 2019",
     endDate: "August 2021",
-    description: "Managed a team of 10+ employees to ensure smooth operation of the store."
+    bulletPoints: [
+      "Managed and trained employees to ensure quality customer service.",
+      "Maintained a safe work environment by properly cleaning and maintaining equipment."
+    ]
   },
   {
     title: "Security Monitor",
     employer: "University of Minnesota",
     startDate: "October 2021",
     endDate: "May 2022",
-    description: "Kept constant, concise, and meticulous communication via logging and radio discussion.\
-    Continuous surveillance and campus building patrols to ensure security and safety for both students and faculty.\
-    Aided students with safe travel through UMN’s 24/7 safe-walk program."
+    bulletPoints: [
+      "Kept constant, concise, and meticulous communication via logging and radio discussion.",
+      "Continuous surveillance and campus building patrols to ensure security and safety for both students and faculty.",
+      "Aided students with safe travel through UMN’s 24/7 safe-walk program."
+    ]
   },
   {
     title: "Firmware Engineering Intern",
     employer: "Seagate Technology",
     startDate: "May 2022",
     endDate: "December 2022",
-    description: "Developed, improved, and tested controller firmware for HDD products.\
-    Effectively utilized engineering strategies while addressing problems to improve adaptability and robustness of code.\
-    Frequent discussion with stakeholders to find the ideal implementation of systems."
+    bulletPoints: [
+      "Developed, improved, and tested controller firmware for HDD products.",
+      "Effectively utilized engineering strategies while addressing problems to improve adaptability and robustness of code.",
+      "Frequent discussion with stakeholders to find the ideal implementation of systems."
+    ]
   }
 ];
 
@@ -40,7 +47,12 @@ function TimelineEvent( props )
       <div className="timeline-event-content">
         <h2>{ props.title }</h2>
         <h3>{ props.employer }</h3>
-        <p>{ props.description }</p>
+        <ul>
+          { console.log( props.bulletPoints )}
+          { props.bulletPoints.map( ( bulletPoint ) => (
+              ( <li>{ bulletPoint }</li> )
+          ))}
+        </ul>
       </div>
       { isEventIndexEven ? <></> : <h3 className="timeline-date-range">{ props.startDate } - { props.endDate }</h3> }
       
@@ -62,7 +74,7 @@ function Timeline( props )
             employer={ event.employer }
             startDate={ event.startDate }
             endDate={ event.endDate }
-            description={ event.description }
+            bulletPoints={ event.bulletPoints }
           />
         ))}
       </div>
