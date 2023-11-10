19c80f377c63859f1a1b99b2c62c707974a5ea75
Jace Halvorson <jacehalvorson33@gmail.com>
2023-11-10 00:39:16 -0600

    Separate functionality into frequency loops based on constants

--- src/conway/conway.jsx
+++ src/conway/conway.jsx
@@ -4,9 +4,14 @@ import './conway.css'
 import { getNextIteration } from './next_iteration';
 import { GameBoard, CELL_HEIGHT, CELL_WIDTH } from './game_board';
 
+const MILLISECONDS_PER_SECOND = 1000;
+
 const NUM_ROWS = 40;
 const NUM_COLS = 40;
 const PLAY_BUTTON_INDEX = 1;
+
+const TICKS_PER_SECOND = 1;
+const GAME_ITERATIONS_PER_SECOND = 2;
 
 function getRandomBoard( )
 {
@@ -60,7 +65,33 @@ function Conway( )
       resetBoard( );
    }, [ resetBoard ] );
 
-   // start the game
+   // Frequency loop of TICKS_PER_SECOND
+   useEffect( ( ) =>
+   {
+      let interval = setInterval( ( ) =>
+      {
+            // TEMPORARY - Move the board around
+            setOffsetX( previousValue => previousValue + ( Math.floor( Math.random() * 50 ) - 25 ) );
+            setOffsetY( previousValue => previousValue + ( Math.floor( Math.random() * 50 ) - 25 ) );
+
+            // Every once in a while, reset offsets
+            if ( Math.random() > 0.95 )
+            {
+               setOffsetX( 200 );
+               setOffsetY( 200 );
+            }
+
+            console.log( `offsetX: ${offsetX}, offsetY: ${offsetY}` );
+      }, MILLISECONDS_PER_SECOND / TICKS_PER_SECOND );
+
+      return ( ) =>
+      {
+         clearInterval( interval );
+      }
+   }, [ isPaused, setOffsetX, setOffsetY, offsetX, offsetY ] );
+
+
+   // Frequency loop of GAME_ITERATIONS_PER_SECOND
    useEffect( ( ) =>
    {
       let interval = setInterval( ( ) =>
@@ -68,13 +99,8 @@ function Conway( )
          if ( !isPaused )
          {
             setGameBoard( previousGameBoard => getNextIteration( previousGameBoard ) );
-
-            // TEMPORARY - Move the board around
-            setOffsetX( previousValue => previousValue + ( Math.floor( Math.random() * 50 ) - 25 ) );
-            setOffsetY( previousValue => previousValue + ( Math.floor( Math.random() * 50 ) - 25 ) );
-            console.log( `offsetX: ${offsetX}, offsetY: ${offsetY}` );
          }
-      }, 1000);
+      }, MILLISECONDS_PER_SECOND / GAME_ITERATIONS_PER_SECOND );
 
       return ( ) =>
       {
