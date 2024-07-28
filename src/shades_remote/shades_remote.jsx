b8a26512ac5c435df7bb986ad4d1d838cd1d5978
Jace Halvorson <jacehalvorson33@gmail.com>
2024-07-28 15:11:35 -0500

    Add GET request to query current state and display

--- src/shades_remote/shades_remote.jsx
+++ src/shades_remote/shades_remote.jsx
@@ -1,36 +1,33 @@
+import {useState} from "react";
 import "./shades_remote.css";
 
 const ip_address = '10.0.0.219';
 const port = '1024';
 
-const connectToWebSocket = () => {
-    const socket = new WebSocket('ws://' + ip_address + ':' + port);
-    socket.addEventListener("open", (event) =>
-    {
-        console.log('Connected to WebSocket');
-    });
-    socket.addEventListener("message", (event) =>
-    {
-        console.log('Message from server: ', event.data);
-    });
-    socket.addEventListener('close', () => {
-        console.log('Disconnected from WebSocket');
-    });
-    socket.addEventListener('error', (err) => {
-        console.log(err);
-    });
-}
-
 function ShadesRemote( )
 {
+    const [shadesState, setShadesState] = useState(0);
+
+    const getShadesState = () => {
+        fetch('http://' + ip_address + ':' + port)
+            .then(response => {console.log(response); return response.json();})
+            .then(data => {
+                setShadesState(data.state);
+            })
+            .catch(err => {
+                console.error('Error reading shades state from ' + ip_address + ':' + port + '\n\n' + err);
+            });
+    }
+
     return (
         <main id="shades-remote">
             <h1>Shades Remote</h1>
-            <button onClick={connectToWebSocket}>Connect</button>
+            <button onClick={getShadesState}>Connect</button>
             <button>Open</button>
             <button>Close</button>
             <button>Enable important mode</button>
             <button>Disable important mode</button>
+            <h2>{shadesState}</h2>
         </main>
     );
 }
