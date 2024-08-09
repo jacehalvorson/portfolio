905d81cf59f929d0527cfecdcb0e70379e731a8f
Jace Halvorson <jacehalvorson33@gmail.com>
2024-08-08 23:02:24 -0500

    Add post requests for actions open, close, important mode

--- src/shades_remote/shades_remote.jsx
+++ src/shades_remote/shades_remote.jsx
@@ -1,35 +1,47 @@
-import {useState} from "react";
+import {useState, useEffect} from "react";
 import "./shades_remote.css";
 
 const ip_address = '10.0.0.219';
-const port = '1024';
+
+function sendPostRequest( parameter )
+{
+    const options = {
+        method: 'POST'
+    }
+
+    fetch('http://' + ip_address + '/' + parameter, options)
+        .then(response => {console.log(response)})
+        .catch(err => {
+            console.error('Error opening shades at ' + ip_address + '\n\n' + err);
+        });
+}
 
 function ShadesRemote( )
 {
-    const [shadesState, setShadesState] = useState(0);
+    const [shadesState, setShadesState] = useState('Loading...');
 
-    const getShadesState = () => {
-        fetch('http://' + ip_address + ':' + port)
+    useEffect( ( ) => {
+        fetch('http://' + ip_address)
             .then(response => {console.log(response); return response.json();})
             .then(data => {
                 setShadesState(data.state);
             })
             .catch(err => {
-                console.error('Error reading shades state from ' + ip_address + ':' + port + '\n\n' + err);
+                console.error('Error reading shades state from ' + ip_address + '\n\n' + err);
             });
-    }
+    }, [ ] );
 
     return (
         <main id="shades-remote">
             <h1>Shades Remote</h1>
-            <button onClick={getShadesState}>Connect</button>
-            <button>Open</button>
-            <button>Close</button>
-            <button>Enable important mode</button>
-            <button>Disable important mode</button>
+            <button onClick={(e) => {sendPostRequest('open')}}>Open</button>
+            <button onClick={(e) => {sendPostRequest('close')}}>Close</button>
+            <button onClick={(e) => {sendPostRequest('important_mode_on')}}>Enable important mode</button>
+            <button onClick={(e) => {sendPostRequest('important_mode_ff')}}>Disable important mode</button>
             <h2>{shadesState}</h2>
         </main>
     );
 }
+
 
 export default ShadesRemote;
