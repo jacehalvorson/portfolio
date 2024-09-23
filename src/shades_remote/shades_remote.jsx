2c82956d08273ddca7f66743cbc79c715c60f850
Jace Halvorson <jacehalvorson33@gmail.com>
2024-09-23 18:23:44 -0500

    Revert "Install MUI components"
    This reverts commit 27047082bfc142ec7a95f98c1d89a3b33812b08e.

--- src/shades_remote/shades_remote.jsx
+++ src/shades_remote/shades_remote.jsx
@@ -1,10 +1,5 @@
-import * as React from 'react';
-import Stack from '@mui/material/Stack';
-import Button from '@mui/material/Button';
-import ToggleButton from '@mui/material/ToggleButton';
-import CheckIcon from '@mui/icons-material/Check';
+import {useState, useEffect} from "react";
 import "./shades_remote.css";
-import { whistle } from 'fontawesome';
 
 const ip_address = '10.0.0.219';
 
@@ -23,47 +18,26 @@ function sendPostRequest( parameter )
 
 function ShadesRemote( )
 {
-    const [shadesState, setShadesState] = React.useState('Loading...');
-    const [selected, setSelected] = React.useState(false);
+    const [shadesState, setShadesState] = useState('Loading...');
 
-    // useEffect( ( ) => {
-    //     fetch('http://' + ip_address)
-    //         .then(response => {console.log(response); return response.json();})
-    //         .then(data => {
-    //             setShadesState(data.state);
-    //         })
-    //         .catch(err => {
-    //             console.error('Error reading shades state from ' + ip_address + '\n\n' + err);
-    //         });
-    // }, [ ] );
+    useEffect( ( ) => {
+        fetch('http://' + ip_address)
+            .then(response => {console.log(response); return response.json();})
+            .then(data => {
+                setShadesState(data.state);
+            })
+            .catch(err => {
+                console.error('Error reading shades state from ' + ip_address + '\n\n' + err);
+            });
+    }, [ ] );
 
     return (
         <main id="shades-remote">
             <h1>Shades Remote</h1>
-            <Stack spacing={2} direction="column">
-                <Button variant="contained" onClick={(e) => {sendPostRequest('open')}}>Open</Button>
-                <Button variant="contained" onClick={(e) => {sendPostRequest('close')}}>Close</Button>
-                <h2>Important Mode:</h2>
-                <ToggleButton
-                    value="check"
-                    selected={selected}
-                    onChange={(e) => {setSelected(!selected)}}
-                    sx={{
-                        bgcolor: "black",
-                        color: "white",
-                        "&.Mui-selected": {
-                            bgcolor: "white",
-                            color: "black",
-                        },
-                        "&.Mui-selected:hover": {
-                            bgcolor: "black",
-                            color: "white",
-                        }
-                    }}
-                >
-                    <CheckIcon />
-                </ToggleButton>
-            </Stack>
+            <button onClick={(e) => {sendPostRequest('open')}}>Open</button>
+            <button onClick={(e) => {sendPostRequest('close')}}>Close</button>
+            <button onClick={(e) => {sendPostRequest('important_mode_on')}}>Enable important mode</button>
+            <button onClick={(e) => {sendPostRequest('important_mode_ff')}}>Disable important mode</button>
             <h2>{shadesState}</h2>
         </main>
     );
