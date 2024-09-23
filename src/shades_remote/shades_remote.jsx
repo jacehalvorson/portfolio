a73f8993268707a60ff8180d354fbbe8e3ca4aa6
Jace Halvorson <jacehalvorson33@gmail.com>
2024-09-23 18:23:53 -0500

    Revert "Add post requests for actions open, close, important mode"
    This reverts commit 905d81cf59f929d0527cfecdcb0e70379e731a8f.

--- src/shades_remote/shades_remote.jsx
+++ src/shades_remote/shades_remote.jsx
@@ -1,47 +1,35 @@
-import {useState, useEffect} from "react";
+import {useState} from "react";
 import "./shades_remote.css";
 
 const ip_address = '10.0.0.219';
-
-function sendPostRequest( parameter )
-{
-    const options = {
-        method: 'POST'
-    }
-
-    fetch('http://' + ip_address + '/' + parameter, options)
-        .then(response => {console.log(response)})
-        .catch(err => {
-            console.error('Error opening shades at ' + ip_address + '\n\n' + err);
-        });
-}
+const port = '1024';
 
 function ShadesRemote( )
 {
-    const [shadesState, setShadesState] = useState('Loading...');
+    const [shadesState, setShadesState] = useState(0);
 
-    useEffect( ( ) => {
-        fetch('http://' + ip_address)
+    const getShadesState = () => {
+        fetch('http://' + ip_address + ':' + port)
             .then(response => {console.log(response); return response.json();})
             .then(data => {
                 setShadesState(data.state);
             })
             .catch(err => {
-                console.error('Error reading shades state from ' + ip_address + '\n\n' + err);
+                console.error('Error reading shades state from ' + ip_address + ':' + port + '\n\n' + err);
             });
-    }, [ ] );
+    }
 
     return (
         <main id="shades-remote">
             <h1>Shades Remote</h1>
-            <button onClick={(e) => {sendPostRequest('open')}}>Open</button>
-            <button onClick={(e) => {sendPostRequest('close')}}>Close</button>
-            <button onClick={(e) => {sendPostRequest('important_mode_on')}}>Enable important mode</button>
-            <button onClick={(e) => {sendPostRequest('important_mode_ff')}}>Disable important mode</button>
+            <button onClick={getShadesState}>Connect</button>
+            <button>Open</button>
+            <button>Close</button>
+            <button>Enable important mode</button>
+            <button>Disable important mode</button>
             <h2>{shadesState}</h2>
         </main>
     );
 }
-
 
 export default ShadesRemote;
