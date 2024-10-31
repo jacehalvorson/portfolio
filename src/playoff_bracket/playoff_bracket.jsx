29eed5e283f04b0195a89fcd1912f7a54a669bd7
trenthalvorson <157232060+trenthalvorson@users.noreply.github.com>
2024-10-31 15:43:51 -0500

    Test Device IDs

--- src/playoff_bracket/playoff_bracket.jsx
+++ src/playoff_bracket/playoff_bracket.jsx
@@ -75,6 +75,25 @@ console.log("Device: " + deviceId);
 
    return (
       <main id="playoff-bracket">
+         <a href={"/playoffbracket/entry?deviceID=" + getOrCreateDeviceId() } 
+		 >
+            <div id="back-button">
+               {/* Left arrow icon */}
+               <svg
+                  id="home-arrow-right"
+                  className="home-arrow-icon"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="1em"
+                  height="1em"
+                  viewBox="0 0 24 24"
+                  >
+                  <path d="m4.431 12.822l13 9A1 1 0 0 0 19 21V3a1 1 0 0 0-1.569-.823l-13 9a1.003 1.003 0 0 0 0 1.645z"/>
+               </svg>
+               <h2>Picks</h2>
+
+            </div>
+         </a>
+
          <h1>{ CurrentYear() } Playoff Bracket Leaderboard</h1>
          
          <div className="leaderboard">
