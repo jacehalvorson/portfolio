9ee63ceecf8e83f6601f8c138764da51610ac5f5
Jace Halvorson <jacehalvorson33@gmail.com>
2024-11-02 08:42:06 -0500

    Add toggle buttons for navigation between pages

--- src/playoff_bracket/playoff_bracket.jsx
+++ src/playoff_bracket/playoff_bracket.jsx
@@ -1,14 +1,21 @@
+import React from "react";
 import {CurrentYear} from "./script.js";
-// import PlayoffBracketLeaderboard from "./playoff_bracket_leaderboard.jsx";
+
+import PlayoffBracketLeaderboard from "./playoff_bracket_leaderboard.jsx";
 import PlayoffBracketPicks from "./playoff_bracket_picks.jsx";
+import PlayoffBracketEntry from "./playoff_bracket_entry.jsx";
+
+import ToggleButton from '@mui/material/ToggleButton';
+import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
+
 import "./playoff_bracket.css";
 import "../index.css";
 
-function getOrCreateDeviceId() 
+function getOrCreateDeviceId( ) 
 {
   let deviceId = localStorage.getItem('deviceId');
   if (!deviceId) {
-      deviceId = Math.random().toString(36).substring(2, 9); // Generate a random string
+      deviceId = Math.random( ).toString(36).substring(2, 9); // Generate a random string
       localStorage.setItem('deviceId', deviceId);
   }
   return deviceId;
@@ -16,35 +23,60 @@ function getOrCreateDeviceId()
 
 function PlayoffBracket( )
 {
+   // Values: 'leaderboard', 'picks', 'admin'
+   const [focus, setFocus] = React.useState('picks');
+   
    // Get the device ID which is comman for a single person.
-   const deviceId = getOrCreateDeviceId();
+   const deviceId = getOrCreateDeviceId( );
    console.log("Device: " + deviceId);
+
+   const switchFocus = (event, newFocus) => {
+      if (newFocus !== null)
+      {
+         setFocus(newFocus);
+      }
+   }
 
    return (
       <main id="playoff-bracket">
-         <a href={"/playoffbracket/entry?deviceID=" + getOrCreateDeviceId() } 
-         >
-            <div id="back-button">
-               {/* Left arrow icon */}
-               <svg
-                  id="home-arrow-right"
-                  className="home-arrow-icon"
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="1em"
-                  height="1em"
-                  viewBox="0 0 24 24"
-                  >
-                  <path d="m4.431 12.822l13 9A1 1 0 0 0 19 21V3a1 1 0 0 0-1.569-.823l-13 9a1.003 1.003 0 0 0 0 1.645z"/>
-               </svg>
-               <h2>Picks</h2>
-
-            </div>
-         </a>
-
          <h1>{ CurrentYear() } Playoff Bracket</h1>
 
-         <PlayoffBracketPicks />
-         
+         <div id="focus-selection-group">
+            <ToggleButtonGroup
+               onChange={switchFocus}
+               value={focus}
+               exclusive
+               sx={{bgcolor: "white"}}
+               aria-label="select-focus"
+            >
+               <ToggleButton
+                  value="leaderboard"
+                  aria-label="leaderboard button"
+               >
+                  Leaderboard
+               </ToggleButton>
+               <ToggleButton
+                  value="picks"
+                  aria-label="picks button"
+               >
+                  Picks
+               </ToggleButton>
+               <ToggleButton
+                  value="admin"
+                  aria-label="admin button"
+               >
+                  Admin
+               </ToggleButton>
+            </ToggleButtonGroup>
+         </div>
+
+         {( focus == "picks" )
+            ? <PlayoffBracketPicks />
+            : ( focus == "leaderboard" )
+               ? <PlayoffBracketLeaderboard />
+               : <PlayoffBracketEntry deviceId={deviceId}/>
+         }
+
          <div id="playoff-bracket-background-picture" />
       </main>
    );
