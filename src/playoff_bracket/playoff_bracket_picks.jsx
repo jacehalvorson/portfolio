b1b9c37e3cbbf5991f6c59481233a1cad86ca1dd
Jace Halvorson <jacehalvorson33@gmail.com>
2024-11-10 20:28:30 -0600

    Populate divisional and championship games based on picks

--- src/playoff_bracket/playoff_bracket_picks.jsx
+++ src/playoff_bracket/playoff_bracket_picks.jsx
@@ -46,6 +46,15 @@ function PlayoffBracketPicks( props )
 
    const updatePick = ( index, value ) =>
    {
+      if ( isNaN( Number( index ) ) || Number( index ) < 0 || Number( index ) > 12 ||
+           isNaN( Number( value ) ) || Number( value ) < 0 || Number( value ) > 2 )
+      {
+         console.error( "Cannot update pick " + index + " with value " + value );
+         return;
+      }
+
+      // Take the existing picks before and after the index, but replace to value at the index
+      // e.g., "1121" + "2" + "00000"
       let newPicks = picks;
       newPicks = picks.substring(0, index) + value + picks.substring(index + 1);
       setPicks( newPicks );
@@ -54,90 +63,72 @@ function PlayoffBracketPicks( props )
    // Update teams when the page loads
    React.useEffect( ( ) => {
       // If the teams are already loaded, save time by using those.
-      if ( !DISABLE_API_CALL )
-      {
-         API.get( apiName, "/?table=playoffTeams" )
-         .then( response => {
-            var teams = {};
+      if ( DISABLE_API_CALL ) return;
 
-            response.forEach( team => 
+      // Retrieve the teams from the "playoffTeams" DynamoDB table using the API
+      API.get( apiName, "/?table=playoffTeams" )
+      .then( response => {
+         var teams = {};
+
+         response.forEach( team => 
+         {
+            // Only take teams from this year
+            if ( team.year === props.currentYear )
             {
-               // Only take teams from this year
-               if ( team.year === props.currentYear )
-               {
-                  teams[team.position] = {
-                     name: team.team,
-                     seed: Number(team.position[1])
-                  };
-               }
-            });
-         
-            setPlayoffTeams( teams );
-         })
-         .catch( err => {
-            console.log( "Error fetching teams from API and parsing" );
-            console.error( err );
-         })
-      }
+               teams[team.position] = {
+                  name: team.team,
+                  seed: Number( team.position[1] )
+               };
+            }
+         });
+      
+         setPlayoffTeams( teams );
+      })
+      .catch( err => {
+         console.log( "Error fetching teams from API and parsing" );
+         console.error( err );
+      })
    }, [ props.currentYear ] );
 
-   // When the teams update, re-create wild card games
+   // Update all the Wild Card games when the playoff teams or picks change
    React.useEffect( ( ) => {
       // Make a list of NFC Wild Card teams that play each other (2 & 7, 3 & 6, 4 & 5)
-      setNfcWildcardGames( [
-         {
-            homeTeam: playoffTeams[ "N2" ],
-            awayTeam: playoffTeams[ "N7" ],
-            winner: Number(picks[0])
-         },
-         {
-            homeTeam: playoffTeams[ "N3" ],
-            awayTeam: playoffTeams[ "N6" ],
-            winner: Number(picks[1])
-         },
-         {
-            homeTeam: playoffTeams[ "N4" ],
-            awayTeam: playoffTeams[ "N5" ],
-            winner: Number(picks[2])
-         }
-      ]);
+      setNfcWildcardGames( computeWildcardGames( playoffTeams, "N", picks.substring( 0, 3 ) ) );
 
       // Make a list of AFC Wild Card teams that play each other (2 & 7, 3 & 6, 4 & 5)
-      setAfcWildcardGames( [
-         {
-            homeTeam: playoffTeams[ "A2" ],
-            awayTeam: playoffTeams[ "A7" ],
-            winner: Number(picks[3])
-         },
-         {
-            homeTeam: playoffTeams[ "A3" ],
-            awayTeam: playoffTeams[ "A6" ],
-            winner: Number(picks[4])
-         },
-         {
-            homeTeam: playoffTeams[ "A4" ],
-            awayTeam: playoffTeams[ "A5" ],
-            winner: Number(picks[5])
-         }
-      ]);
-
-      // Default divisonal games to have just the 1 seed for each conference
-      setNfcDivisionalGames( [
-         { homeTeam: playoffTeams[ "N1" ], awayTeam: null, winner: Number(picks[6]) },
-         { homeTeam: null, awayTeam: null, winner: Number(picks[7])}
-      ]);
-      setAfcDivisionalGames ( [
-         { homeTeam: playoffTeams[ "A1" ], awayTeam: null, winner: Number(picks[8]) },
-         { homeTeam: null, awayTeam: null, winner: Number(picks[9])}
-      ]);
+      setAfcWildcardGames( computeWildcardGames( playoffTeams, "A", picks.substring( 3, 6 ) ) );
    }, [ playoffTeams, picks ] );
+
+   // Update the NFC Divisional games when Wild Card games update
+   React.useEffect( ( ) =>
+   {
+      setNfcDivisionalGames( computeDivisionalGames( nfcWildcardGames, playoffTeams[ "N1" ], picks.substring( 6, 8 ) ) );
+   }, [ nfcWildcardGames, playoffTeams, picks ] );
+      
+   // Update the AFC Divisional games when Wild Card games update
+   React.useEffect( ( ) =>
+   {
+      setAfcDivisionalGames( computeDivisionalGames( afcWildcardGames, playoffTeams[ "A1" ], picks.substring( 8, 10 ) ) );
+   }, [ afcWildcardGames, playoffTeams, picks ] );
+
+   // Update the AFC Championship when Divisional Games update
+   React.useEffect( ( ) =>
+   {
+      setAfcChampionship( computeChampionshipGame( afcDivisionalGames, picks.substring( 11, 12 ) ) );
+   }, [ afcDivisionalGames, picks ] );
+
+   // Update the NFC Championship when Divisional Games update
+   React.useEffect( ( ) =>
+   {
+      setNfcChampionship( computeChampionshipGame( nfcDivisionalGames, picks.substring( 10, 11 ) ) );
+   }, [ nfcDivisionalGames, picks ] );
 
    return (
       <div id="playoff-bracket-picks">
          {/* NFC Wild Card */
          nfcWildcardGames.map( ( game, index ) =>
             <PlayoffBracketGame
-               gridRow={ ( 2 * index + 1) + " / span 2" }
+               gridRow={ ( 2 * index + 1 ) + " / span 2" }
                gridColumn="7"
                game={game}
                key={index}
@@ -149,7 +140,7 @@ function PlayoffBracketPicks( props )
          {/* AFC Wild Card */
          afcWildcardGames.map( ( game, index ) =>
             <PlayoffBracketGame
-               gridRow={ ( 2 * index + 1) + " / span 2" }
+               gridRow={ ( 2 * index + 1 ) + " / span 2" }
                gridColumn="1"
                game={game}
                key={index}
@@ -161,7 +152,7 @@ function PlayoffBracketPicks( props )
          {/* NFC Divisional */
          nfcDivisionalGames.map( ( game, index ) =>
             <PlayoffBracketGame
-               gridRow={ ( 2 * index + 2) + " / span 2" }
+               gridRow={ ( 2 * index + 2 ) + " / span 2" }
                gridColumn="6"
                game={game}
                key={index}
@@ -173,7 +164,7 @@ function PlayoffBracketPicks( props )
          {/* AFC Divisional */
          afcDivisionalGames.map( ( game, index ) =>
             <PlayoffBracketGame
-               gridRow={ ( 2 * index + 2) + " / span 2" }
+               gridRow={ ( 2 * index + 2 ) + " / span 2" }
                gridColumn="2"
                game={game}
                key={index}
@@ -281,6 +272,127 @@ function PlayoffBracketGame( props )
          }
       </ToggleButtonGroup>
    )
+}
+
+// Given the wildcard games and the picks, compute the divisional games.
+// conference is either "N" or "A" for NFC or AFC.
+// wildcardPicks is a string with 3 characters. e.g., "011" means the first game is
+// unpicked and the second and third games are picked for the home team, where the
+// first game is defined as the highest seed playing the lowest seed.
+function computeWildcardGames( playoffTeams, conference, wildcardPicks )
+{
+   return [
+   {
+      homeTeam: playoffTeams[ conference + "2" ],
+      awayTeam: playoffTeams[ conference + "7" ],
+      winner: Number(wildcardPicks[0])
+   },
+   {
+      homeTeam: playoffTeams[ conference + "3" ],
+      awayTeam: playoffTeams[ conference + "6" ],
+      winner: Number(wildcardPicks[1])
+   },
+   {
+      homeTeam: playoffTeams[ conference + "4" ],
+      awayTeam: playoffTeams[ conference + "5" ],
+      winner: Number(wildcardPicks[2])
+   }];
+}
+
+// Given the wildcard games, the bye team, and the picks, compute the divisional games.
+// divisionalPicks is a string with 2 charactes. e.g., "01" means the first game is
+// unpicked and the second game is picked for the home team, where the first game
+// is defined as the highest seed playing the lowest seed.
+function computeDivisionalGames( wildcardGames, byeTeam, divisionalPicks )
+{
+   let divisionalTeams = [ byeTeam ];
+   let divisionalGames = [ ];
+
+   wildcardGames.forEach( game =>
+   {
+      if ( game.winner === 1 )
+      {
+         divisionalTeams.push( game.homeTeam );
+      }
+      else if ( game.winner === 2 )
+      {
+         divisionalTeams.push( game.awayTeam );
+      }
+      // For games with no team picked (0), do nothing
+   });
+
+   // Sort teams by seed
+   divisionalTeams.sort( ( a, b ) => a.seed - b.seed );
+
+   // Repeatedly take the highest and lowest seeds to play each other
+   // until there are no more teams left
+   while ( divisionalTeams.length > 0 )
+   {
+      divisionalGames.push({
+         // Home team is the highest seed
+         homeTeam: divisionalTeams[0],
+         // Away team is the lowest seed or use null if there is no other team
+         awayTeam: ( divisionalTeams.length > 1 )
+            ? divisionalTeams.at( -1 )
+            : null,
+         // Leave win prediction blank
+         winner: 0
+      });
+
+      // Take off the teams just used
+      if ( divisionalTeams.length > 1 )
+         divisionalTeams = divisionalTeams.slice( 1, ( divisionalTeams.length - 1 ) );
+      else
+         divisionalTeams = [ ];
+   }
+
+   // If there is only one game, add an empty one
+   if ( divisionalGames.length === 1 )
+   {
+      divisionalGames.push( emptyGame );
+   }
+
+   // Incorporate picks into the divisional games
+   divisionalGames[0].winner = Number( divisionalPicks[0] );
+   divisionalGames[1].winner = Number( divisionalPicks[1] );
+
+   return divisionalGames;
+}
+
+// Given the divisional games and the championship pick, compute the championship game
+// championshipPick is a string with 1 character. e.g., "1" means the home team won.
+function computeChampionshipGame( divisionalGames, championshipPick )
+{
+   let championshipTeams = [ ];
+
+   divisionalGames.forEach( game =>
+   {
+      if ( game.winner === 1 && game.homeTeam )
+      {
+         championshipTeams.push( game.homeTeam );
+      }
+      else if ( game.winner === 2 && game.awayTeam )
+      {
+         championshipTeams.push( game.awayTeam );
+      }
+      // For games with no team picked (0), do nothing
+   });
+
+   if ( championshipTeams.length === 0 )
+   {
+      return emptyGame;
+   }
+
+   // Sort teams by seed
+   championshipTeams.sort( ( a, b ) => a.seed - b.seed );
+
+   return {
+      homeTeam: championshipTeams[0],
+      awayTeam: ( championshipTeams.length > 1 )
+         ? championshipTeams[1]
+         : null,
+      winner: Number( championshipPick )
+   };
 }
 
 export default PlayoffBracketPicks;
