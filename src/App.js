2fd7c772e044c4ec7c6eb85f7be4d8810de7bbcd
trenthalvorson <157232060+trenthalvorson@users.noreply.github.com>
2024-12-15 19:20:29 -0600

    Allow games be recorded
    Allow a small number of people to keep score how games are playing.

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Product from './ecommerce/product.jsx';
 import Checkout from './ecommerce/checkout.jsx';
 import NFLStats from './stat_huddle/stat_huddle.jsx';
 import PlayoffBracket from './playoff_bracket/playoff_bracket.jsx';
+import PaymentGameWin from './playoff_bracket/PaymentGameWin.jsx';
 
 function App( )
 {
@@ -20,6 +21,7 @@ function App( )
         <Route path="/ecommerce/checkout" element={ <Checkout /> } />
         <Route path="/stathuddle" element={ <NFLStats /> } />
         <Route path="/playoffbracket" element={ <PlayoffBracket /> } />
+        <Route path="/paymentgamewin" element={<PaymentGameWin />} />
         <Route path="*" element={<h1>404: Not Found</h1>} />
       </Routes>
     </Router>
