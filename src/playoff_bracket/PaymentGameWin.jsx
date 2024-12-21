3e304d63aec9eb165d73576b650f5adc073c03ef
trenthalvorson <157232060+trenthalvorson@users.noreply.github.com>
2024-12-21 11:53:28 -0600

    Give names to divisions
    This can happen after winning wildcard games are complete.

--- src/playoff_bracket/PaymentGameWin.jsx
+++ src/playoff_bracket/PaymentGameWin.jsx
@@ -63,6 +63,12 @@ function PaymentGameWin( )
     var spanColorA26 = "white";
     var spanColorN17 = "white";
     var spanColorN27 = "white";
+    var AF1Second = "M";
+    var AF1Third = "m";
+    var AF1Last = "L";
+    var NF1Second = "M";
+    var NF1Third = "m";
+    var NF1Last = "L";
     try {
         const allGames = computeAllGames(scores);
         if (allGames.afcWildcardGames[0].winner === 1) {
@@ -113,6 +119,11 @@ function PaymentGameWin( )
         if (allGames.afcDivisionalGames[1].winner === 2) {
             spanColorA25 = "red";
         }
+        if (allGames.afcDivisionalGames[0].awayTeam != null && allGames.afcDivisionalGames[1].awayTeam != null) {
+            AF1Second = allGames.afcDivisionalGames[1].homeTeam.charAt(1);
+            AF1Third = allGames.afcDivisionalGames[1].awayTeam.charAt(1);
+            AF1Last = allGames.afcDivisionalGames[0].awayTeam.charAt(1);
+        }
         if (allGames.nfcDivisionalGames[0].winner === 1) {
             spanColorN14 = "red";
         }
@@ -125,27 +136,32 @@ function PaymentGameWin( )
         if (allGames.nfcDivisionalGames[1].winner === 2) {
             spanColorN25 = "red";
         }
-        if (allGames.afcChampionshipGames[0].winner === 1) {
+        if (allGames.nfcDivisionalGames[0].awayTeam != null && allGames.nfcDivisionalGames[1].awayTeam != null) {
+            NF1Second = allGames.nfcDivisionalGames[1].homeTeam.charAt(1);
+            NF1Third = allGames.nfcDivisionalGames[1].awayTeam.charAt(1);
+            NF1Last = allGames.nfcDivisionalGames[0].awayTeam.charAt(1);
+        }
+        if (allGames.afcChampionshipGames && allGames.afcChampionshipGames[0].winner === 1) {
             spanColorA16 = "red";
         }
-        if (allGames.afcChampionshipGames[0].winner === 2) {
+        if (allGames.afcChampionshipGames && allGames.afcChampionshipGames[0].winner === 2) {
             spanColorA26 = "red";
         }
-        if (allGames.nfcChampionshipGames[0].winner === 1) {
+        if (allGames.nfcChampionshipGames && allGames.nfcChampionshipGames[0].winner === 1) {
             spanColorN17 = "red";
         }
-        if (allGames.nfcChampionshipGames[0].winner === 2) {
+        if (allGames.nfcChampionshipGames && allGames.nfcChampionshipGames[0].winner === 2) {
             spanColorN27 = "red";
         }
-    } catch (err) { }
+    } catch (err) { console.error(err); }
 
     return (
         <main id="playoff-bracket-entry">
             <h2>{CurrentYear()} Playoff Bracket</h2>
             <h2>Picks (NFL_BRACKET)</h2>
             <h4>---------AFC NFC AF NF A N S</h4>
-            <div>Line 1 - <span id={spanColorA11}>2</span><span id={spanColorA12}>3</span><span id={spanColorA13}>4</span> <span id={spanColorN11}>2</span><span id={spanColorN12}>3</span><span id={spanColorN13}>4</span> <span id={spanColorA14}>1</span><span id={spanColorA15}>M</span> <span id={spanColorN14}>1</span><span id={spanColorN15}>M</span> <span id={spanColorA16}>H</span> <span id={spanColorN17}>H</span> A</div>
-            <div>Line 2 - <span id={spanColorA21}>7</span><span id={spanColorA22}>6</span><span id={spanColorA23}>5</span> <span id={spanColorN21}>7</span><span id={spanColorN22}>6</span><span id={spanColorN23}>5</span> <span id={spanColorA24}>L</span><span id={spanColorA25}>m</span> <span id={spanColorN24}>L</span><span id={spanColorN25}>m</span> <span id={spanColorA26}>L</span> <span id={spanColorN27}>L</span> N</div>
+            <div>Line 1 - <span id={spanColorA11}>2</span><span id={spanColorA12}>3</span><span id={spanColorA13}>4</span> <span id={spanColorN11}>2</span><span id={spanColorN12}>3</span><span id={spanColorN13}>4</span> <span id={spanColorA14}>1</span><span id={spanColorA15}>{AF1Second}</span> <span id={spanColorN14}>1</span><span id={spanColorN15}>{NF1Second}</span> <span id={spanColorA16}>H</span> <span id={spanColorN17}>H</span> A</div>
+            <div>Line 2 - <span id={spanColorA21}>7</span><span id={spanColorA22}>6</span><span id={spanColorA23}>5</span> <span id={spanColorN21}>7</span><span id={spanColorN22}>6</span><span id={spanColorN23}>5</span> <span id={spanColorA24}>{AF1Last}</span><span id={spanColorA25}>{AF1Third}</span> <span id={spanColorN24}>{NF1Last}</span><span id={spanColorN25}>{NF1Third}</span> <span id={spanColorA26}>L</span> <span id={spanColorN27}>L</span> N</div>
             <input type="text" id="newPicks" defaultValue={scores} />
             <button
                 id="add-to-NFL-bracket"
